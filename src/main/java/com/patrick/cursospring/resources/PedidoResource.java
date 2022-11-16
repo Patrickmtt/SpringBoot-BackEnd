94eda7abb0929a3d927b8f13e1c5e67206d03f04
Patrick Matheus <patrickmt111@gmail.com>
2022-11-16 18:28:39 -0300

    Configuração do SQL - Heroku

--- src/main/java/com/patrick/cursospring/resources/PedidoResource.java
+++ src/main/java/com/patrick/cursospring/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.patrick.cursospring.domain.Categoria;
 import com.patrick.cursospring.domain.Pedido;
-import com.patrick.cursospring.dto.CategoriaDTO;
 import com.patrick.cursospring.services.PedidoService;
 
 @RestController
